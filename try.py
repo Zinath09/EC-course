a31ee8295b282234f509d438c8d30f0f0bab7238
Zinath09 <zuzabu@o2.pl>
2024-03-10 22:30:13 +0100

    Lab 7 finished with a report

--- try.py
+++
@@ -1,9 +0,0 @@
-lista = [0,1,2,3,4,5,6,7,8,9]
-
-new = 90
-index = 9
-new_lista = lista[:index+1] + [new] +lista[index+1:]
-for i in range(len(lista)):
-    print(i)
-print(lista[11:])
-print(new_lista)
